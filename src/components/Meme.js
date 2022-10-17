2b4213a9d9cd4e643f4c483aa548cf7992507b69
Yamiao Liu <ymliu97@gmail.com>
2022-10-17 15:47:15 -0400

    get url of radom meme images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,27 @@
 import React from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+
+    function getMemeImg() {
+        //define data array
+        const memesArray = memesData.data.memes
+        //get random number that is a whole number and within array range
+        const randomNum = Math.floor(Math.random()*memesArray.length)
+        //get random url
+        const url = memesArray[randomNum].url
+        //test
+        console.log(url)
+    }
+
+
     return (
         <form className="meme--form">
             <div className="meme--input--container">
                 <input className="meme--input" type="text" placeholder="top text field" />
                 <input className="meme--input" type="text" placeholder="bottom text field" />
             </div>
-            <button className="meme--button" type="button">Get a new meme image  🖼</button>
+            <button onClick={getMemeImg} className="meme--button" type="button">Get a new meme image  🖼</button>
         </form>
     )
 }
