fbf1edb5ab52457ff0443db75f1f634ceec53599
Yamiao Liu <ymliu97@gmail.com>
2022-10-17 11:00:38 -0400

    Rewrite css as my own style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,13 @@ import React from "react"
 export default function Header() {
     return (
         <header className="header">
-            <img 
-                src="https://img.icons8.com/doodle/96/000000/trollface.png" 
-                className="header--image"
-            />
-            <h2 className="header--title">Meme Generator</h2>
+            <div className="header--div">
+                <img 
+                    src="https://img.icons8.com/doodle/96/000000/trollface.png" 
+                    className="header--logo"
+                />
+                <h2 className="header--title">Meme Generator</h2>
+            </div>
             <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
