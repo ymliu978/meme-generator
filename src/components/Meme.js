15594f3dea390b1a4f769021402819b9d97c608c
Yamiao Liu <ymliu97@gmail.com>
2022-10-19 12:45:22 -0400

    useState to get meme images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,25 +3,29 @@ import memesData from "../memesData"
 
 export default function Meme() {
 
+    const [memeImage, setMemeImage] = React.useState("")
+
     function getMemeImg() {
         //define data array
         const memesArray = memesData.data.memes
         //get random number that is a whole number and within array range
         const randomNum = Math.floor(Math.random()*memesArray.length)
-        //get random url
-        const url = memesArray[randomNum].url
-        //test
-        console.log(url)
+        //useState
+        setMemeImage(memesArray[randomNum].url)
     }
 
 
     return (
-        <form className="meme--form">
-            <div className="meme--input--container">
-                <input className="meme--input" type="text" placeholder="top text field" />
-                <input className="meme--input" type="text" placeholder="bottom text field" />
-            </div>
-            <button onClick={getMemeImg} className="meme--button" type="button">Get a new meme image  🖼</button>
-        </form>
+        <main className="meme--main--container">
+            <form className="meme--form">
+                <div className="meme--input--container">
+                    <input className="meme--input" type="text" placeholder="top text field" />
+                    <input className="meme--input" type="text" placeholder="bottom text field" />
+                </div>
+                <button onClick={getMemeImg} className="meme--button" type="button">Get a new meme image  🖼</button>
+            </form>
+            <img src={memeImage} className="meme-image"/>
+        </main>
+        
     )
 }
